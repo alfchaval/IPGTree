4a5edf22d8ffe4c95a3db414a0cc5e46d045850d
alfchaval <alfchaval@hotmail.com>
2020-07-03 02:01:55 +0200

    CR

--- app/src/main/java/mtg/judge/ipgtree/POJO/Card.java
+++ app/src/main/java/mtg/judge/ipgtree/POJO/Card.java
@@ -47,7 +47,7 @@ public class Card {
             text += this.manaCost;
         }
         text += "\n\n";
-        if(colorIndicator != null) {
+        if(colorIndicator.size() > 1) {
             text += "Color indicator: ";
             for (String s: colorIndicator) {
                 text += s;
